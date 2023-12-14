ef74be03b7f1730988ee4949909a6484f4860adb
Aditya singh <115808044+Adityadeveloper28@users.noreply.github.com>
2023-12-14 10:56:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,13 @@ connectDB();
 ///rest obj
 const app = express();
 //middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin :["https://expert-cv-generator-using-mern.vercel.app"],
+    methods:["POST"."GET"],
+    credentials: true
+  }
+));
 app.use(express.json());
 app.use(morgan("dev"));
 
